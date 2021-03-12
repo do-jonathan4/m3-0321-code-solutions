c938c947682c1e51170029e3b6f6d3f097713a4c
John Do <jdo697@gmail.com>
2021-03-12 15:46:14 -0800

    asdf

---
+++ babel-intro/dist/main.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+var foo = 'bar';
+var i = 0;
+i += 1;
+
+for (var _i = 1; _i <= 10; _i++) {
+  console.log(_i);
+}
+
+var baz = function () {
+  console.log('qux');
+};
+
+baz();
+/******/ })()
+;
