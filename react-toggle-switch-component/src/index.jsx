807fdc286df6976cc9c8bd5523da2d0cd3dffb67
John Do <jdo697@gmail.com>
2021-03-20 12:57:42 -0700

    fixed issue

--- react-toggle-switch-component/src/index.jsx
+++ react-toggle-switch-component/src/index.jsx
@@ -1,41 +1,39 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
 
 class ToggleSwitch extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {isClicked: false}
-    this.flip = this.flip.bind(this)
+    super(props);
+    this.state = { isClicked: false };
+    this.flip = this.flip.bind(this);
   }
+
   flip() {
     this.setState({
       isClicked: !this.state.isClicked
-    })
-   }
-  render () {
-    let isClicked = this.state.isClicked
-    let position;
-    let positionClass;
-    let containerBG;
+    });
+  }
 
-    isClicked ? position = 'On' : position = 'Off'
-    isClicked ? positionClass = 'on slider' : positionClass = 'slider'
-    isClicked ? containerBG = 'container-slider-on' : containerBG = 'container-slider'
+  render() {
+    const isClicked = this.state.isClicked;
+    const position = isClicked ? 'On' : 'Off';
+    const positionClass = isClicked ? 'on slider' : 'slider';
+    const containerBG = isClicked ? 'container-slider-on' : 'container-slider';
 
     return (
-      <div class='container'>
-        <div class={containerBG}>
-          <div onClick={this.flip} class={positionClass}></div>
+      <div className='container'>
+        <div className={containerBG}>
+          <div onClick={this.flip} className={positionClass}></div>
         </div>
-        <div class='container'>
+        <div className='container'>
           <h2>{position}</h2>
         </div>
       </div>
-    )
+    );
   }
 }
 
 ReactDom.render(
   <ToggleSwitch />,
   document.getElementById('root')
-)
+);
