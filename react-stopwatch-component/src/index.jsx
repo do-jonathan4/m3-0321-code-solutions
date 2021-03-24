b21ce785b9a86415bf0a3ff95d233ca2a0a88311
John Do <jdo697@gmail.com>
2021-03-23 22:00:15 -0700

    completed react-stopwatch-component

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -4,32 +4,37 @@ import ReactDOM from 'react-dom'
 class StopWatch extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {num: 0}
-    this.state = {tick: false}
+    this.state = {
+      isTick: false,
+      num: 0
+    }
     this.count = this.count.bind(this)
+    this.reset = this.reset.bind(this)
   }
   count() {
-    this.setState({
-      tick: !this.state.tick
-    })
+    this.setState({ isTick: !this.state.isTick})
+
+    if (!this.state.isTick) {
+      this.intervalId = setInterval(() => {
+        this.setState({ num: this.state.num + 1})
+      }, 1000)
+    } else {
+      clearInterval(this.intervalId)
+    }
   }
-  stop() {
-    if (!this.state.tick) {
-      this.setState({
-        num: 0
-      })
+  reset() {
+    if (!this.state.isTick) {
+      this.setState({ num: 0 })
     }
   }
   render() {
-    const num = this.state.num
-    const tick = this.state.tick
-
-    const pausePlay = tick ? 'play' : 'pause'
+    const num = this.state.num < 10 ? '0' + this.state.num : this.state.num
+    const pausePlay = this.state.isTick ? 'fas fa-pause fa-5x' : 'fas fa-play fa-5x'
 
     return (
       <div>
-        <div id='face' onClick={this.stop}></div>
-        <div id='icon-container' onClick={this.count}>{pausePlay}</div>
+        <div id='face' onClick={this.reset}>{num}</div>
+        <div id='icon-container' onClick={this.count} class={pausePlay}></div>
       </div>
     )
   }
